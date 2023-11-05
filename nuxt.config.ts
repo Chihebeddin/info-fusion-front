d6f2eb4b93ddefb31e02541a77dc26af1a628b71
BIVEGUE Isabelle <isabelle.biveguengoa.etu@univ-lille.fr>
2023-11-05 16:52:10 +0100

    Règles css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  modules : [
+    '@vee-validate/nuxt',
+
+  ],
+  veeValidate : {
+    autoImports: true,
+    componentNames: {
+      Form: 'VeeForm',
+      Field: 'VeeField',
+      FieldArray: 'VeeFieldArray', 
+      ErrorMessage: 'VeeErrorMessage',
+    },
+  }
+
 })

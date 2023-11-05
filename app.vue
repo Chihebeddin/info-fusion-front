74de16f3981d7bf45a279df665980bc1fa5c228a
BIVEGUE Isabelle <isabelle.biveguengoa.etu@univ-lille.fr>
2023-11-05 16:54:33 +0100

    Affichage des layouts et des pages

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+    <!--
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/signin">Connexion</NuxtLink></li>
+          <li><NuxtLink to="/signup">Inscription</NuxtLink></li>
+        </ul>
+      </nav>
+    </header> -->
   </div>
 </template>

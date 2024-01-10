86d46e1086cc064c6e994e746aceb0dfbe221e5a
BIVEGUE Isabelle <isabelle.biveguengoa.etu@univ-lille.fr>
2024-01-10 20:11:34 +0100

    Liste des magasins

--- pages/UserProfile.vue
+++ pages/UserProfile.vue
@@ -1,35 +1,77 @@
 <template>
   <div>
-    <br><br><br>
-    <h1>User Profile</h1>
-    <p>{{ store.token }}</p>
-    <ul>
-      <li v-for="item in userDataList" :key="item.key">
-        <strong>{{ item.key }}:</strong> {{ item.value }}
-      </li>
-    </ul>
+    <section class="bg-gray-50 dark:bg-gray-900">
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <Splide
+          aria-labelledby="autoplay-example-heading"
+          :options="options"
+          :has-track="false"
+        >
+          <SplideTrack>
+            <SplideSlide v-for="shop in shops" :key="shop.id" data-splide-interval="5000">
+              <NuxtLink to="/customers/shop">
+                <img class="rounded-t-lg" src="../assets/images/grande_surface.jpg" alt="Grande surface">
+                <div class="py-4">
+                  <div class="font-bold text-xl mb-2">
+                    {{ shop.name }}
+                  </div>
+                </div>
+              </NuxtLink>
+            </SplideSlide>
+          </SplideTrack>
+        </Splide>
+      </div>
+    </section>
   </div>
 </template>
 
-<script setup>
-import { onMounted, ref } from 'vue'
-import { useAuthStore } from '../store/auth.module.ts'
+<script>
+import axios from 'axios'
+import _ from 'lodash'
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/vue-splide'
+import '@splidejs/vue-splide/css/sea-green'
 
-const store = useAuthStore()
-const userDataList = ref([])
+export default {
+  name: 'Shops',
+  components: {
+    Splide,
+    SplideTrack,
+    SplideSlide
+  },
+  setup () {
+    useHead({
+      title: 'Accueil'
+    })
 
-const onRefresh = () => {
-  console.log(store.user)
-  console.log(store.token)
+    const options = {
+      rewind: true,
+      gap: '1rem',
+      autoplay: true,
+      height: '15rem',
+      perPage: 3,
+      perMove: 1,
+      width: '55%'
+    }
+
+    return {
+      options
+    }
+  },
+  data () {
+    return {
+      shops: {}
+    }
+  },
+  mounted () {
+    this.getShopsList()
+  },
+  methods: {
+    getShopsList () {
+      axios.get('http://localhost:8080/shops').then((res) => {
+        this.shops = _.shuffle(res.data).slice(0, 10)
+      })
+    }
+  }
 }
 
-onMounted(async () => {
-  store.init()
-  const response = await store.fetchUserInfo()
-
-  const userEntries = Object.entries(response.data)
-  userDataList.value = userEntries.map(([key, value]) => ({ key, value }))
-
-  onRefresh()
-})
 </script>

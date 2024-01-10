56a138b819f853648c29589c3c9dc0e991fc6bd7
BIVEGUE Isabelle <isabelle.biveguengoa.etu@univ-lille.fr>
2024-01-10 20:11:55 +0100

    Configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
+  app: {
+    head: {
+      title: 'ShopLoc',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
+    }
+  },
+
   devtools: { enabled: true },
 
   css: ['~/assets/css/main.css'],

7db105eb5a5a22920be61d7d90b91bb29e806b96
BIVEGUE Isabelle <isabelle.biveguengoa.etu@univ-lille.fr>
2024-02-02 20:05:27 +0100

    Handle shopping cart

--- pages/customers/shops/[id].vue
+++ pages/customers/shops/[id].vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="bg-gray-light">
     <section class="bg-gray-50 dark:bg-gray-900">
       <div class="py-8 px-4 mx-auto max-w-screen-2xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
         <div class="flex flex-col justify-center">
@@ -34,23 +34,27 @@
       </div>
     </section>
     <section class="container mx-auto">
-      <div class="w-2/4 max-w-fit grid grid-cols-3 gap-6">
-        <div v-for="product in products" :key="product.id" cm>
-          <div class="h-90 bg-white border border-gray-light shadow dark:bg-white dark:border-gray-400">
-            <a href="#">
-              <img src="../../../assets/images/baguette.jpg" alt="product image">
-            </a>
+      <div class="w-3/5 max-w-fit grid grid-cols-4 gap-6">
+        <div v-for="product in products" :key="product.id">
+          <div class="h-90 bg-white border border-gray-light rounded-sm shadow dark:bg-white dark:border-gray-400">
+            <div href="#">
+              <img class="h-48" src="../../../assets/images/baguette.jpg" alt="product image">
+            </div>
             <div class="mx-3">
-              <div class="mt-2 mb-1 h-24 text-ellipsis overflow-hidden">
-                <h1 class="font-semibold leading-6 text-xl text-wrap">
+              <div class="mt-2 mb-1 h-16">
+                <h1 class="font-semibold leading-6 text-lg tracking-tight">
                   {{ product.name }}
                 </h1>
               </div>
               <div class="mb-1">
-                <span class="text-xl font-medium text-gray">{{ product.price }} €</span>
+                <span class="text-lg font-medium text-gray">{{ product.price }} €</span>
               </div>
               <div class="mb-2">
-                <button type="submit" class="w-full text-teal hover:text-teal-700 hover:bg-gray-light focus:ring-4 focus:outline-none focus:ring-gray-light font-medium rounded-md text-2xl px-5 py-1 text-center dark:bg-gray-light dark:hover:bg-gray-400 dark:focus:ring-gray-light">
+                <button
+                  type="submit"
+                  class="w-full text-teal border border-gray-light hover:text-teal-700 hover:border-gray-400 focus:ring-1 focus:outline-none focus:ring-gray-light font-medium rounded-sm text-2xl px-5 py-1 text-center dark:bg-gray-light dark:focus:ring-gray-light"
+                  @click="addToCart(product)"
+                >
                   +
                 </button>
               </div>
@@ -58,8 +62,8 @@
           </div>
         </div>
       </div>
-      <div class="fixed top-64 right-0 z-40 w-64 h-screen pt-5 transition-transform -translate-x-full sm:translate-x-0"">
-        <ShoppingCart />
+      <div class="fixed top-74 right-10 z-40 w-1/3 max-w-auto h-screen pt-5 transition-transform -translate-x-full sm:translate-x-0">
+        <ShoppingCart :items="itemsSelected" />
       </div>
     </section>
   </div>
@@ -74,7 +78,8 @@ export default {
     return {
       shopId: '',
       shop: '',
-      products: ref([])
+      products: ref([]),
+      itemsSelected: ref([])
     }
   },
   mounted () {
@@ -92,6 +97,19 @@ export default {
       await axios.get(`http://localhost:8080/products/filtered?shop=${shopId}`).then((res) => {
         this.products = res.data
       })
+    },
+    addToCart (elt) {
+      const updatedElt = this.itemsSelected.find(item => item.id === elt.id)
+      if (updatedElt !== undefined) {
+        updatedElt.quantity++
+      } else {
+        this.itemsSelected.push({
+          id: elt.id,
+          name: elt.name,
+          price: elt.price,
+          quantity: 1
+        })
+      }
     }
   }
 }
